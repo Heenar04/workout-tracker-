3bf27367269aa88a719ef60dcf096d3e18b3abe3
hrajpal <hrajpal@ebrevia.com>
2021-02-01 11:22:29 -0500

    dashboard update

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,26 @@ app.get("/api/workouts", (req, res) => {
         console.log(err)
     });
   })
-  
+  // for 7 days range 
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+      {
+        $addFields:{
+          totalduration : {
+            $sum : "$exercises.duration"
+          }
+        }
+      }
+    ]).sort({_id : -1}).limit(7)    // limiting the object to 7 
+    .then(data => {
+      
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+  })
+
   // Insert a workout
   app.post("/api/workouts", (req,res) => {
     db.Workout.create({}).then((data) => {
